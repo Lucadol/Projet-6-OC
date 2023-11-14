e3c4d991acaa35810529f28696d71df171291e34
Lucas Leib <lucas.leib@gmail.com>
2023-11-14 18:29:29 +0100

    feat : add page A propos

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -1,7 +1,31 @@
+import fond from '../assets/Apropos_fond_2.png'
+import './Apropos.scss'
+import { IoIosArrowUp } from "react-icons/io";
+
+
+const carouselData = [
+    "Fiabilité",
+    "Respect",
+    "Service",
+    "Sécurité",
+  ];
+  
+  const carousel = carouselData.map((text, index) => (
+    <div key={index} className="card_carousel">
+      {text}
+      <IoIosArrowUp className='arrow-icon'/>
+    </div>
+  ));
+
 export function Apropos () {
     return (
-        <div>
-            A propos
-        </div>
+        <>
+              <div className='fond'>
+                <img className="paysage" src={fond} alt="photo paysage" />
+            </div>
+            <div className='carousel'>
+                    {carousel}
+                </div>
+        </>
     )
 }
