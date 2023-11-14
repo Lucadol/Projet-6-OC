d93fea9077d67a6ea43361e119ebcac774318536
Lucas Leib <lucas.leib@gmail.com>
2023-11-14 18:29:44 +0100

    feat : add page Error

--- src/pages/PageError.jsx
+++ src/pages/PageError.jsx
@@ -1,5 +1,18 @@
+import { NavLink } from 'react-router-dom'
+import './PageError.scss'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+
 export function PageError() {
     return <>
-    <h1>Une erreur est survenue</h1>
+      <Header/>
+      <div className='Error404'>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n&apos;existe pas</p>
+        <NavLink to='/' className="link">
+          Retourner sur la page d&apos;accueil
+        </NavLink>
+      </div>
+      <Footer/>
     </>
   }
