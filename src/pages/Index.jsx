6b4d42ce373ee609e3430a189ac289f5f67f350c
Lucas Leib <lucas.leib@gmail.com>
2023-11-14 18:28:55 +0100

    feat : work on index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,23 @@
+import fond from '../assets/home_fond_2.png'
+import './Index.scss'
+
+const cards = Array.from({ length: 6 }, (_, index) => (
+    <div key={index} className="card_logement"></div>
+  ));
+
 export function Index () {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <div className='fond'>
+                <h1 className='texte_paysage'>Chez vous, partout et ailleurs</h1>
+                <img className="paysage" src={fond} alt="photo paysage" />
+            </div>
+
+            <div className='container'>
+                <div className='container_logements'>
+                    {cards}
+                </div>
+            </div>
+        </>
     )
 }
