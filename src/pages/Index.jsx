04b500c5ce289d5af6f813e3c3d1dde5baeb1d77
Lucas Leib <lucas.leib@gmail.com>
2023-11-20 16:29:31 +0100

    feat : add home cards with json

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,22 @@
 import fond from '../assets/home_fond_2.png'
 import './Index.scss'
-
-const cards = Array.from({ length: 6 }, (_, index) => (
-    <div key={index} className="card_logement"></div>
-  ));
+import LogementsJson from '../../logements.json'
 
 export function Index () {
+
+    // Trouve le logement correspondant à l'ID dans le fichier JSON
+    const logements = LogementsJson;
+
+    const cards = logements.map((logement) => (
+        <a key={logement.id} href={`/logements/${logement.id}`} className="card_logement">
+            <div className='card_inner'>
+                <img src={logement.cover} alt="{logement.title}" className='card_image' />
+                <div className='card_overlay'></div>
+                <div className='card_title'>{logement.title}</div>
+            </div>
+        </a>
+      ))
+
     return (
         <>
             <div className='fond'>
