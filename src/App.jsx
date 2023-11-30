faa3fbd6fa058d24069986084d9fcd72e0bff8e5
Lucas Leib <lucas.leib@gmail.com>
2023-11-30 18:49:42 +0100

    fix : delete root.jsx and update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import { Logements } from "./pages/Logements/Logements"
 import { Apropos } from "./pages/Apropos/Apropos"
 import { Index } from "./pages/Index/Index"
 import { PageError } from "./pages/PageError/PageError"
-import { Root } from "./utils/Root"
+import { Outlet } from "react-router-dom"
+import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
 import './App.scss'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root/>,
+    element: (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    ),
     errorElement: <PageError/>,
     children: [
       {
